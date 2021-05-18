c39c8ea0d57f063b42e46517652186512e3d5441
Maycon de Souza <Parasyyh0408!>
2021-05-18 14:24:45 -0300

    Added first test to MessageService

---
+++ pdi-consumer/src/test/java/com/abinbev/bees/pdiconsumer/PdiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abinbev.bees.pdiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdiConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- pdi-consumer/src/test/java/com/abinbev/bees/pdiconsumer/service/MessageServiceTest.java
+++ pdi-consumer/src/test/java/com/abinbev/bees/pdiconsumer/service/MessageServiceTest.java
@@ -1,31 +1,44 @@
 package com.abinbev.bees.pdiconsumer.service;
 
 import com.abinbev.bees.pdiconsumer.domain.Message;
+import com.abinbev.bees.pdiconsumer.repository.MessageRepository;
 import com.abinbev.bees.pdiconsumer.services.MessageService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 @ExtendWith(MockitoExtension.class)
 public class MessageServiceTest {
+
+    @Mock
+    MessageRepository repository;
 
     @InjectMocks
     private MessageService service;
 
     @Test
-    private List<Message> whenListMessagesIsCalledThenReturnAListMessages() {
+    void  whenListMessagesIsCalledThenReturnAListMessages() {
         //given
-
+        Message expectedMessage = new Message("123456789", "teste message");
 
         //when
-
-
+        Mockito.when(repository.findAll()).thenReturn(Collections.singletonList(expectedMessage));
 
         //then
-        return new ArrayList<Message>();
+        List<Message> foundedMessages = service.findAll();
+
+        assertThat(foundedMessages, is(not(empty())));
+        assertThat(foundedMessages.get(0),is(equalTo(expectedMessage)));
     }
 }
