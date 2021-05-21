f85682e9ef210654ce3a2d2f6303f0caf87ecb23
Maycon de Souza <Parasyyh0408!>
2021-05-21 14:56:05 -0300

    Initial commit for producer

---
+++ pdi-producer/src/main/java/com/abinbev/bees/pdiproducer/PdiProducerApplication.java
@@ -0,0 +1,13 @@
+package com.abinbev.bees.pdiproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdiProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdiProducerApplication.class, args);
+	}
+
+}
